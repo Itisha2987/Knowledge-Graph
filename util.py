d437ebbe126daccd980fd9d896b2001e0661bd29
unknown <itishapatidar1@gmail.com>
2020-11-23 17:31:13 +0530

    Added method to find inferences and deductions from a node

--- util.py
+++ util.py
@@ -19,9 +19,45 @@ def get_instinctive_activations(node, graph):
     return activations
 
 
+def get_observations(node,graph):
+    '''
+        Returns list of possible inferences
+        and deductions from the given node,
+        usign bfs traversal.
+    '''
+
+    # Set level of traversal as 2.
+    level = 2
+    queue = []
+    queue.append(node)
+    observations = {}
+    l = 1
+
+    while queue:
+        if l > level:
+            return observations
+        size = len(queue)
+
+        for i in range(size):
+            n = queue.pop(0)
+            for s in list(graph.successors(n)):
+                edge_weight = int(graph[n][s]['weight'])
+                # edge weight 2 implies inference while edge weight 3 implies deductions.
+                if edge_weight == 2 or edge_weight == 3:
+                    queue.append(s)
+                    # add observation if not already included or is an inference.
+                    if s not in observations or observations[s] == 2:
+                        observations[s] = int(graph[n][s]['weight'])
+
+        l = l+1
+    return observations
+
+
 if __name__=="__main__":
     graph = KnowledgeGraph()
     kn_graph = graph.build_graph()
 
     activations = get_instinctive_activations('tangy_smell', kn_graph)
     print(activations)
+    observations = get_observations('spoon', kn_graph)
+    print(observations)
