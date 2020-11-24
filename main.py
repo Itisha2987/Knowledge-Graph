805de2ec5ef63b61d124b94d0457bec296c13a4c
Karan Modh <karanmodh71@gmail.com>
2020-11-24 16:03:04 +0530

    Updated main file and added comments

--- main.py
+++ main.py
@@ -1,50 +1,79 @@
 from build_knowledge_graph import KnowledgeGraph
 from util import get_instinctive_activations
 from util import get_observations
+from util import is_dangling_node
+from util import get_parent_nodes
+from util import seperate_observations_and_predictions
 
 
 def main():
 	graph = KnowledgeGraph().build_graph()
 	instinctive_reactions = {}
-	observations_and_predictions = {}
-	final_results = {}
+	observations = {}
+	predictions = {}
+	object_recognized = {}
 	inputs = []
-	visited_nodes = []
+
 	while(True):
+
+		# Take User Input until user enters exit	
 		sensory_inputs = input("\nEnter your sensory inputs: \n")
 		if sensory_inputs=="exit":
 			break
 
-		observation_for_given_level = {}
+		observations_for_given_level = {}
+		predictions_for_given_level = {}
 		instinctive_reactions_for_given_level = {}
-		results_for_given_level = {}
+		object_recognitions_for_given_level = {}
 
-		inputs.extend(sensory_inputs.strip().split(' '))
+		# new_inputs the user has entered
+		new_inputs = sensory_inputs.strip().split(' ')
+
+		# adding new user inputs in inputs set
+		inputs.extend(new_inputs)
+		
+		'''
+			for new_inputs check for dangling node
+			if dangling node add its parents to objects_recognized set
+		'''
+		for new_sensory_input in new_inputs:
+			if is_dangling_node(new_sensory_input, graph):
+				object_recognitions_for_given_level.update(get_parent_nodes(new_sensory_input, graph))
+
+		object_recognized.update(object_recognitions_for_given_level)
+
+		# For each input get activations, observations and predictions
 		for sensory_input in inputs:
-			if sensory_input in visited_nodes:
-				continue
+			
+			# Activations
 			activations = get_instinctive_activations(sensory_input, graph)
 			instinctive_reactions_for_given_level.update(activations)
 			instinctive_reactions.update(activations)
-			observations, results = get_observations(sensory_input, graph)
-			# print(observations)
-			results_for_given_level.update(results)
-			final_results.update(results)
-			observation_for_given_level.update(observations)
-			observations_and_predictions.update(observations)
 
-		inputs = list(observation_for_given_level)
+			# Observations
+			raised_observations = get_observations(sensory_input, graph)
+			observations_for_given_level.update(raised_observations)
+			observations.update(raised_observations)
 
-		print("Observations for given level: \n", observation_for_given_level)
-		print("Results for given level: \n", results_for_given_level)
-		print("Instinctive Reactions for given level: \n", instinctive_reactions_for_given_level)
+		# Adding old observations to input set
+		inputs = list(observations_for_given_level)
+
+		# Separating observations and predictions
+		observations_for_given_level, predictions_for_given_level = seperate_observations_and_predictions(observations_for_given_level)
+		
+		print("\nObservations for given level: \n", observations_for_given_level)
+		print("\nPredictions for given level: \n", predictions_for_given_level)
+		print("\nObjects Recognized for given level: \n", object_recognitions_for_given_level)
+		print("\nInstinctive Reactions for given level: \n", instinctive_reactions_for_given_level)
 
 	print("---------------------------------------------------")
-	print("Final Observations \n", observations_and_predictions)
+	print("Final Observations \n", observations)
+	print("---------------------------------------------------")
+	print("Final Predictions \n", predictions)
 	print("---------------------------------------------------")
 	print("Final Instinctive Reactions \n", instinctive_reactions)
 	print("---------------------------------------------------")
-	print("Final Results \n", final_results)
+	print("Final Object Recognized \n", object_recognized)
 	print("---------------------------------------------------")
 
 
